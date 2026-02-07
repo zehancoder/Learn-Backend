1b39e04b9ee03a9cd00df6d43770d712118ef03b
Jonaeed Hossen <zehanjh93@gmail.com>
2026-02-08 00:54:47 +0600

    Update MongoDB connection string in server.js
    Removed sensitive MongoDB connection string.

--- day6/server.js
+++ day6/server.js
@@ -2,7 +2,7 @@ const app = require('./src/app')
 /// use mongoose for connect with mongodb database/cluster
 const mongoose = require('mongoose')
 function connectTodb() {
-    mongoose.connect('mongodb+srv://zehan:v2f0Yie7uktTfCHo@cluster0.9zklzw6.mongodb.net/day-6')// link from mongodb conpass copy connection string
+    mongoose.connect('')// link from mongodb conpass copy connection string
     .then(() => { 
         console.log('connected to database'); // after connect log
         
